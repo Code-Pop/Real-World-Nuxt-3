708fc391f8a2c9d3a7f30fb48c8eabbd5cc6ff3d
Andy <andy@Andys-MacBook-Pro.local>
2024-01-09 21:50:43 -0500

    render markdown

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import type { Post } from '@/data/posts'
+import { getPostsUrl } from '@/data/posts'
+
+const { data: posts } = await useFetch<Post[]>(getPostsUrl())
+</script>
+
 <template>
-  <h1>Hello World</h1>
+  <main>
+    <h1>Nuxt.js Examples</h1>
+    <div>
+      <div
+        v-for="post in posts"
+        :key="post.id"
+      >
+        <h2>
+          {{ post.title }}
+        </h2>
+        <div>
+          <a href="#">Category: {{ post.category.name }}</a>
+        </div>
+        <RenderMarkdown :source="post.intro" />
+      </div>
+    </div>
+  </main>
 </template>
