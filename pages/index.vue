e882b64755bc33f0466f9c791eed478afee21245
Andy <andy@Andys-MacBook-Pro.local>
2024-01-09 21:50:43 -0500

    render markdown

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import type { Post } from "@/data/posts";
+import { getPostsUrl } from "@/data/posts";
+
+const { data: posts } = await useFetch<Post[]>(getPostsUrl());
+</script>
+
 <template>
   <main>
     <h1>Nuxt.js Examples</h1>
