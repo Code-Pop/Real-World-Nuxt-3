a129210819629d0d600b8757c16131ed3484050b
Andy <andy@Andys-MacBook-Pro.local>
2024-02-07 09:27:55 -0500

    category link

---
+++ components/CategoryLink.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { Category } from '@/data/categories'
+
+const { category } = defineProps<{ category: Category }>()
+</script>
+
+<template>
+  <div class="category">
+    <a href="#">Category: {{ category.name }}</a>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/styles/colors.scss';
+
+.category {
+  display: block;
+  margin: 15px 0px;
+}
+
+.category a {
+  color: colors.$green-medium;
+  border: 1px solid colors.$green-dark;
+  border-radius: 5px;
+  font-size: 12pt;
+  padding: 5px 10px;
+}
+
+.category a:hover {
+  color: colors.$green-light;
+  border-color: colors.$green-medium;
+}
+</style>
--- components/PostCard.vue
+++ components/PostCard.vue
@@ -2,17 +2,25 @@
 import type { Post } from '@/data/posts'
 
 const { post } = defineProps<{ post: Post }>()
+
+const isHover = ref(false)
 </script>
 
 <template>
-  <div class="card">
+  <div class="card" :class="{hover: isHover}">
     <h2 class="title">
       {{ post.title }}
     </h2>
-    <div class="category">
-      <a href="#">Category: {{ post.category.name }}</a>
-    </div>
+    <CategoryLink :category="post.category" />
     <RenderMarkdown :source="post.intro" />
+    <a
+      class="more"
+      href="#"
+      @mouseenter="isHover = true"
+      @mouseleave="isHover = false"
+    >
+      More ...
+    </a>
   </div>
 </template>
 
@@ -20,18 +28,36 @@ const { post } = defineProps<{ post: Post }>()
 @use "@/assets/styles/colors.scss";
 
 .card {
-  padding: 15px;
+  padding: 15px 15px 35px;
   border: 1px solid colors.$green-dark;
   border-radius: 5px;
   margin-bottom: 25px;
   font-size: 12pt;
+  position: relative;
 }
 
 .card .title {
   font-size: 20pt;
 }
 
-.card .category {
-  margin: 5px 0px;
+.card .more {
+  color: colors.$green-medium;
+  font-size: 12pt;
+  padding: 5px 10px;
+  position: absolute;
+  right: 0px;
+  bottom: 0px;
+}
+
+.card.hover {
+  border-color: colors.$green-medium;
+}
+
+.card.hover .title {
+  color: colors.$green-light;
+}
+
+.card.hover .more {
+  color: colors.$green-light;
 }
 </style>
