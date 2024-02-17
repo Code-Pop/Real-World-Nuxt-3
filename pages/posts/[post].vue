db93e23bb827f1552651b93813a701957899c270
Andy <andy@Andys-MacBook-Pro.local>
2024-02-16 19:50:32 -0500

    template v-if

---
+++ pages/posts/[post].vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { PostDetails } from '@/data/posts'
+import { getPostDetailsUrl } from '@/data/posts'
+
+const postSlug = useRoute().params.post.toString()
+
+const { data: post } = await useFetch<PostDetails>(
+  getPostDetailsUrl(postSlug)
+)
+</script>
+
+<template>
+  <main>
+    <template v-if="post">
+      <h1>
+        {{ post.title }}
+        <CategoryLink :category="post.category" />
+      </h1>
+      <RenderMarkdown :source="post.content" />
+    </template>
+  </main>
+</template>
